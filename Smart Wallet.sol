ea48e5bdc02af71be24e93b30d064e324ec0ac40
startup-dreamer <s_kbharbhunja@me.iitr.ac.in>
2022-09-30 03:39:25 +0530

    smart wallet with solidity

--- Smart Wallet.sol
+++ Smart Wallet.sol
@@ -1,47 +1,42 @@
-pragma solidity ^0.6.0;
+pragma solidity >=0.5.0 <0.9.0;
 
-contract SmartWallet {
+// Adding Ownable openzapplin library for Owner controls
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-    mapping(address => uint) Balance;
+// Adding Safemath library to prevent uint wrapping
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-    address payable public owner;
-
-    event RecievedMoney(address _address, uint _amount);
-
-    uint private funds;
-
-    bool register; 
+contract Allowance is Ownable {
     
+    using SafeMath for uint;
 
-    constructor() public {
-        owner = msg.sender;  
+    mapping(address => uint) Recipent;
+    // created modifier which verifies user is either allowed or the owner
+    modifier Owner_or_Allowed(uint _amount) {
+        require(msg.sender == owner() || Recipent[msg.sender] >= _amount, "You are not authorised");
+        _;
+    }
+    // event triggers when allowance alloted
+    event AllowanceSet(address indexed _beneficiary, uint _amount);
+    // event triggers when allowance is reduced 
+    event ReducedAllowance(address indexed _whom, address indexed _who, uint _prevAmount, uint _newAmount);
+    // Function to set the allowance for recipents
+    function SetAllowance(address _to, uint _amount) public onlyOwner(){
+        Recipent[_to] = _amount;
+        emit AllowanceSet(_to, _amount);
+    }
+    // functioin to reduce allowance either by spending money or by owner
+    function ReduceAllowance(address _who, uint _amount) public Owner_or_Allowed(_amount) {
+        Recipent[_who] = Recipent[_who].sub(_amount);
+        emit ReducedAllowance(msg.sender, _who, Recipent[_who].add(_amount), Recipent[_who]);
+    }
+    // function overriden to prevent ownership transfer
+    function transferOwnership(address newOwner) public override onlyOwner {
+        revert("You are not permitted for this permission");
+    }
+    // function overriden to prevent new ownership
+    function renounceOwnership() public override onlyOwner {
+        revert("You are not permitted for this permission");
     }
 
-    fallback() payable external {
-        emit RecievedMoney(msg.sender, msg.value / 1 ether);
-        Balance[owner] += msg.value;
-    }
-
-    function Register() public returns(bool) {
-        if(owner == msg.sender){
-            return register = true;
-        }
-        else{
-            Balance[msg.sender] = 20;
-            return register = true;
-        }
-        
-    }
-
-    function WithdrawMoney(uint _amount) payable public {
-        require(register == true || Balance[msg.sender] == Balance[owner], "You are not registered");
-        require(Balance[msg.sender] >= _amount, "Not enough remaining money on wallet");
-        require(Balance[owner] >= _amount, "Not enough funds on wallet");
-        assert(Balance[owner] - _amount <= Balance[owner]);
-        assert(Balance[msg.sender] - _amount <= Balance[msg.sender]);
-        payable(msg.sender).transfer(_amount * 1 ether);
-        Balance[msg.sender] -= _amount;
-        Balance[owner] -= _amount;
-    }
 }
-
